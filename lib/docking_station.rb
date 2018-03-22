e3c39fc382f26c2e380f19c29a8cbab6e38d7717
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-03-22 15:20:13 +0000

    step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,15 +3,26 @@ require_relative 'bike'
 class DockingStation
 
   attr_reader :bike
+  attr_reader :collection
+
+  def initialize
+    @collection = []
+  end
 
   def release_bike
-    fail 'No bike available' unless @bike
+    fail 'No bike available' unless @collection.count > 0
     Bike.new
+    @collection.pop
   end
 
   def dock(bike)
-    fail "Docking station is full" if @bike
+    fail "Docking station is full" if @collection.count == 20
     @bike = bike
+    @collection.push(@bike)
   end
 
 end
+
+# station = DockingStation.new
+# p 19.times { station.dock Bike.new }
+# p @collection
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,31 +5,33 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  # it 'releases working bikes' do
-  #   bike = subject.release_bike
-  #   expect(bike).to be_working
-  # end
+  it 'releases working bikes' do
+    subject.dock Bike.new
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 
   # it 'docks something' do
   #   bike = Bike.new
   #   expect(subject.dock(bike)).to eq bike
   # end
 
-  # it 'return docked bikes' do
-  #   bike = Bike.new
-  #   subject.dock(bike)
-  #   expect(subject.bike).to eq bike
-  # end
+  it 'return docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
   describe '#release_bike' do
     it 'raises an error' do
+      # 20.times { subject.dock Bike.new }
       expect { subject.release_bike }.to raise_error ("No bike available")
     end
   end
 
   describe '#dock' do
     it ' raises an error when full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock Bike.new }
       expect { subject.dock("bike") }.to raise_error ("Docking station is full")
     end
   end
